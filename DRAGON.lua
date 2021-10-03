08a68526be89f96a6c95a26048ad19b460b9a496
Team IDK <91576573+IDK-Zizo@users.noreply.github.com>
2021-10-03 21:49:17 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -13480,31 +13480,22 @@ send(msg.chat_id_, msg.id_,Text)
 end
 return false
 end
-if SourceCh(msg) and text ==  الاحصائيات  then
+if SourceCh(msg) and text ==  'الاحصائيات'  then
 if Sudo(msg) then 
 local Groups = bot_data:scard(bot_id.. Chek:Groups )  
 local Users = bot_data:scard(bot_id.. User_Bot )  
 keyboard = {} 
 keyboard.inline_keyboard = {
 {
-{text = ✯ عدد الجروبات  ➠ { ..Groups.. } .. 
+{text = '✯ عدد الجروبات  ➠ { '..Groups..' } '
 },
 {
-{text = ✯  عدد المشتركين  ➠ { ..Users.. } 
+{text = '✯  عدد المشتركين  ➠ { '..Users..' } '
 },
 }
 local function getpro(extra, result, success) 
 if result.photos_[0] then 
 https.request("https://api.telegram.org/bot"..token.. /sendPhoto?chat_id=  .. msg.chat_id_ ..  &photo= ..result.photos_[0].sizes_[1].photo_.persistent_id_.. &caption=  .. URL.escape(Namebot).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
-end
-if text == 'المشتركين' then
-if Sudo(msg) then 
-local Groups = database:scard(bot_id..'Chek:Groups')  
-local Users = database:scard(bot_id..'User_Bot')  
-Text = ' ♫ عدد المشتركين  ⇇{`'..Users..'|}'
-send(msg.chat_id_, msg.id_,Text) 
-end
-return false
 end
 if text == 'تفعيل المغادره' and DevSoFi(msg) then   
 if database:get(bot_id..'Left:Bot'..msg.chat_id_) then
